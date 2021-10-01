2dd0206fd747574850ec377283d220a40b2528cd
Diego <dcandido@ciandt.com>
2021-10-01 16:56:38 -0300

    Finalizando Projetos

--- product-catalog/src/main/java/br/com/diego/productcatalog/ProductCatalogApplication.java
+++ product-catalog/src/main/java/br/com/diego/productcatalog/ProductCatalogApplication.java
@@ -2,8 +2,12 @@ package br.com.diego.productcatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class ProductCatalogApplication {
 
 	public static void main(String[] args) {
